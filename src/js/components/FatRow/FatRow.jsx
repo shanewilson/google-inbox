2e203aac39bc9f35ac6704c28ebbe20d91a53ecb
Shane Wilson <shane.wilson@oicr.on.ca>
2015-06-24 11:18:29 -0400

    chore(FatRow): hide type errors

--- src/js/components/FatRow/FatRow.jsx
+++ src/js/components/FatRow/FatRow.jsx
@@ -2,11 +2,11 @@
 
 import React from "react/addons";
 
-function sum(numbers: Array<number>): string {
-  return 1;
-}
+//function sum(numbers: Array<number>): string {
+//  return 1;
+//}
 
-sum([1, 2, "c"]);
+//sum([1, 2, "c"]);
 
 class FatRow extends React.Component {
   render(): React.Element {

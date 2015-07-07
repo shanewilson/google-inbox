1caf7c79e77907913c1f3e6ff2b702f356996bbf
Shane Wilson <shane.wilson@oicr.on.ca>
2015-07-07 18:18:46 -0400

    flow ignore config

--- scripts/flow-annotation-check.sh
+++ scripts/flow-annotation-check.sh
@@ -2,7 +2,7 @@
 WHITE=$(tput setaf 7)
 NORMAL=$(tput sgr0)
 
-noflow=$(git ls-files | egrep '^src.*\.jsx?$' | grep -v 'test' | xargs grep --files-without-match '@flow')
+noflow=$(git ls-files | egrep '^src.*\.jsx?$' | grep -v 'test' | grep -v 'config' | xargs grep --files-without-match '@flow')
 if [ -n "$noflow" ]; then
   echo "These files are missing ${WHITE}@flow${NORMAL} annotations:"
   echo "$noflow"
